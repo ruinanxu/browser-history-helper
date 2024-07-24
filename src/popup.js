cde62ccff2c862034f2e2e379705b6147091736c
Ruinan Xu <18603769186@163.com>
2024-07-24 17:18:28 +0800

    store tags.

--- src/popup.js
+++ src/popup.js
@@ -1,25 +1,8 @@
 // popup.js - handles interaction with the extension's popup, sends requests to the
 // service worker (background.js), and updates the popup's UI (popup.html) on completion.
 
-const inputElement = document.getElementById('text');
 const outputElement = document.getElementById('output');
 const generateTags = document.getElementById('gen-tags');
-
-// Listen for changes made to the textbox.
-inputElement.addEventListener('input', (event) => {
-
-    // Bundle the input data into a message.
-    const message = {
-        action: 'classify',
-        text: event.target.value,
-    }
-
-    // Send this message to the service worker.
-    chrome.runtime.sendMessage(message, (response) => {
-        // Handle results returned by the service worker (`background.js`) and update the popup's UI.
-        outputElement.innerText = JSON.stringify(response, null, 2);
-    });
-});
 
 generateTags.addEventListener('click', (event) => {
     chrome.history.search({ text: '', maxResults: 5 }, async function (data) {
@@ -31,8 +14,12 @@ generateTags.addEventListener('click', (event) => {
 
             chrome.runtime.sendMessage(message, (response) => {
                 console.log('received user data', response)
-                outputElement.innerText = JSON.stringify(response, null, 2);
+                // outputElement.innerText = JSON.stringify(response, null, 2);
+                const storageKey = page.id;
+                const storageValue = JSON.stringify({ title: page.title, tags: response });
+                localStorage.setItem(storageKey, storageValue);
             });
+
         });
     });
 })
